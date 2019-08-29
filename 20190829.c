3fd99121790f4007a120516ff5491378d4049445
BelongAl <1454776915@qq.com>
2019-08-29 16:55:33 +0800

    See if each string is repeated multiple times by a substring

---
+++ 20190829.c
@@ -0,0 +1,39 @@
+#include<stdio.h> 
+
+int Find (char *arr)
+{
+  char *p = arr;
+  char *q = arr+1;
+  int count = 0;
+  int k = 1;
+  while(*q != '\0')
+  {
+    if(*p != *q)
+    {
+      p = p-count;
+      count = 0;
+      q++;
+      k++;
+    }
+    else 
+    {
+      p++;
+      q++;
+      count++;
+    }
+  }
+  if(k <= count)
+  {
+    return 1;
+  }
+  else 
+  return 0;
+}
+
+int main()
+{
+  char *str = "abcdeabcde";
+  int ret = Find(str);
+  printf("%d\n",ret);
+  return 0;
+}
