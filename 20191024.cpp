6e9ebf3304b36471a65309f4aad7fd37a7ebe2c2
BelongAl <1454776915@qq.com>
2019-10-24 19:57:20 +0800

    Jump Game

--- 20191024.cpp
+++ 20191024.cpp
@@ -6,28 +6,50 @@ using namespace std;
 class Solution {
 public:
 	bool canJump(vector<int>& nums) {
-		for (int i = 0; i < nums.size(); i)
+		if (nums.size() < 2)
 		{
-			if (nums[i] == 0 && i != nums.size() - 1)
+			return true;
+		}
+		int count = nums.size() - 1;
+		bool*ptr = new bool[count--];
+		if (nums[count] > 0)
+		{
+			ptr[count--] = true;
+		}
+		else
+		{
+			ptr[count--] = false;
+		}
+		for (int i = count; i >= 0; i--)
+		{
+			int tmp = nums[i];
+			if ((i + tmp) >= nums.size() - 1)
 			{
-				return false;
+				ptr[i] = true;
 			}
 			else
 			{
-				i += nums[i];
-			}
-			if (i == nums.size() - 1)
-			{
-				return true;
+				int j = 1;
+				for (j; j <= tmp; j++)
+				{
+					if (ptr[i + j] == true)
+					{
+						ptr[i] = true;
+						break;
+					}
+				}
+				if (j > tmp)
+				{
+					ptr[i] = false;
+				}
 			}
 		}
-		return false;
+		return ptr[0];
 	}
 };
-
 int main()
 {
-	int arr[] = {0};
+	int arr[] = { 2, 3, 1, 0, 4 };
 	vector<int> v(arr, arr + 5);
 	Solution s;
 	cout << s.canJump(v) << endl;
