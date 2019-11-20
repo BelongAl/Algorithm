2a6b870603e59df5adfdafbec43601dc08c45592
BelongAl <1454776915@qq.com>
2019-11-20 15:27:19 +0800

    binarry sort tree

--- BinarrySortTree.h
+++ BinarrySortTree.h
@@ -11,7 +11,7 @@ namespace wr
 		TreeNode<T> *m_right;
 	public:
 		TreeNode(const T& val = T()) :
-			m_data(val) ,
+			m_data(val),
 			m_left(nullptr),
 			m_right(nullptr)
 		{}
@@ -25,7 +25,7 @@ namespace wr
 	{
 		TreeNode<T> *m_root;
 	public:
-		BinarySortTree():
+		BinarySortTree() :
 			m_root(nullptr)
 		{}
 
@@ -78,10 +78,10 @@ namespace wr
 				return false;
 			}
 
-			TreeNode<T> *cur = m_root;
-			TreeNode<T> *pre = nullptr;
+			TreeNode<T> *cur = m_root;//标记需要删除的元素
+			TreeNode<T> *pre = nullptr;//标记需要删除元素的父亲
 
-			while (cur)
+			while (cur)//查找需要删除的元素
 			{
 				if (val < cur->m_data)
 				{
@@ -99,31 +99,70 @@ namespace wr
 				}
 			}
 
-			if (cur == nullptr)
+			if (cur == nullptr)//若不存在
 			{
 				return false;
 			}
 
-			if (cur->m_left && cur->m_right)
+			if (cur->m_left && cur->m_right)//需要删除的元素其左右子树都有元素
 			{
-
+				TreeNode<T> *cur2 = cur->m_left;//找到进位的元素
+				TreeNode<T> *pre2 = cur;//标记需要进位的元素上一个元素
+				while (cur2->m_right)
+				{
+					pre2 = cur2;
+					cur2 = cur2->m_right;
+				}
+				if (cur->m_left == cur2)//需要进位的元素就是删除元素的直接左子树
+				{
+					if (pre->m_data > val)
+					{
+						pre->m_left = cur2;
+					}
+					else
+					{
+						pre->m_right = cur2;
+					}
+				}
+				else//需要进位的元素是需要删除元素的左子树中的最大值
+				{
+					pre2->m_right = cur2->m_left;
+					if (pre->m_data > val)
+					{
+						pre->m_left = cur2;
+					}
+					else
+					{
+						pre->m_right = cur2;
+					}
+					cur2->m_left = cur->m_left;
+					cur2->m_right = cur->m_right;
+				}
 			}
-
-			if (cur->left)
+			else if (cur->m_left)//需要删除的元素只有左子树上有元素
 			{
-
+				if (pre->m_data > val)
+				{
+					pre->m_left = cur->m_left;
+				}
+				else
+				{
+					pre->m_right = cur->m_left;
+				}
 			}
-
-			if (cur->m_right)
+			else//需要删除的元素只有在右子树上有元素或两个子树均没有元素
 			{
-
+				if (pre->m_data > val)
+				{
+					pre->m_left = cur->m_right;
+				}
+				else
+				{
+					pre->m_right = cur->m_right;
+				}
 			}
-			else
-			{
-
-				delete cur;
-			}
-
+			delete cur;
+			cur = nullptr;
 			return true;
 		}
 	};
