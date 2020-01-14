36bd7a52f355d5eb93ce7fe7dd1af08927698a6a
BelongAl <1454776915@qq.com>
2020-01-14 17:42:11 +0800

    back pack

--- 202001145.cpp
+++ 202001145.cpp
@@ -4,31 +4,45 @@
 
 using namespace std;
 
-class Solution {
-public:
-	int backPackII(int m, vector<int> &A, vector<int> &V) 
+int backPack(int m, vector<int> &A, vector<int> &V)
+{
+	int num = A.size();
+	vector<int> max_value(m + 1, 0);//存储价值
+	for (int i = 1; i <= num; i++)//控制物品个数
 	{
-		int num = A.size();
-		vector<vector<int>> max_value(num + 1, vector<int>(m+1, 0));
-		for (int i = 1; i <= num; i++)
+		for (int j = m; j > 0; j--)//控制背包大小
 		{
-			for (int j = 1; j <= m; j++)
+			if (A[i - 1] <= j)
 			{
-				if (A[i - 1] <= j)
-				{
-					max_value[i][j] = max(max_value[i - 1][j], max_value[i - 1][j - A[i - 1]] + V[i - 1]);
-				}
-				else
-				{
-					max_value[i][j] = max_value[i - 1][j];
-				}
+				max_value[j] = max(max_value[j], max_value[j - A[i - 1]] + V[i - 1]);
 			}
 		}
-		return max_value[num][m];
 	}
-};
+	return max_value[m];
+}
 
 int main()
 {
+	int N = 5, V = 10;
+
+	vector<int> size;
+	vector<int> value;
+
+	size.push_back(6);
+	size.push_back(4);
+	size.push_back(2);
+	size.push_back(4);
+	size.push_back(3);
+
+	value.push_back(8);
+	value.push_back(10);
+	value.push_back(4);
+	value.push_back(5);
+	value.push_back(5);
+
+	cout << backPack(V, size, value) << endl;;
+
+	system("pause");
+
 	return 0;
 }
