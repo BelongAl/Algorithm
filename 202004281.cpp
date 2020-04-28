204f4b68f6be9d724b5874f9ca07b06fdf05b3e7
BelongAl <1454776915@qq.com>
2020-04-28 19:43:10 +0800

    Unique paths 2

--- 202004281.cpp
+++ 202004281.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 //f[i][j] = f[i-1][j] + f[i][j-1];
 
-class Solution {
+class Soluion {
 public:
 	int uniquePaths(int m, int n) {
 		vector<vector<int>> f(m, vector<int>(n, 0));
@@ -29,10 +29,60 @@ public:
 	}
 };
 
+class Solution {
+public:
+	int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+		if (obstacleGrid.size() == 0)
+		{
+			return 0;
+		}
+		vector<vector<int>> f(obstacleGrid.size(), vector<int>(obstacleGrid[0].size(), 0));
+		for (int i = 0; i < obstacleGrid.size(); i++)
+		{
+			if (obstacleGrid[i][0] == 1)
+			{
+				break;
+			}
+			f[i][0] = 1;
+		}
+		for (int i = 0; i < obstacleGrid[0].size(); i++)
+		{
+			if (obstacleGrid[0][i] == 1)
+			{
+				break;
+			}
+			f[0][i] = 1;
+		}
+		for (int i = 1; i < obstacleGrid.size(); i++)
+		{
+			for (int j = 1; j < obstacleGrid.size(); j++)
+			{
+				if (obstacleGrid[i][j] == 0)
+				{
+					f[i][j] = f[i - 1][j] + f[i][j - 1];
+				}
+			}
+		}
+		return f[f.size() - 1][f[0].size() - 1];
+	}
+};
+
 int main()
 {
 
 	Solution s;
-	int ret = s.uniquePaths(3, 7);
+	vector<vector<int>> v;
+	vector<int> a1{ 0,0,0,1,0 };
+	vector<int> a2{ 0,1,0,0,0 };
+	vector<int> a3{ 0,0,1,0,0 };
+	vector<int> a4{ 0,1,0,0,0 };
+	vector<int> a5{ 0,0,0,1,0 };
+	v.push_back(a1);
+	v.push_back(a2);
+	v.push_back(a3);
+	v.push_back(a4);
+	v.push_back(a5);
+	int ret = s.uniquePathsWithObstacles(v);
+
 	return 0;
 }
